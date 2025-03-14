30305d8cf7e29886a1d2a61ebda939c7d203d92a
Brayan Duque <bradumar2016@gmail.com>
2025-03-14 03:03:12 -0500

    comprobando sintaxis de useState

--- exercisesReact/src/App.jsx
+++ exercisesReact/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Tarjeta from './Tarjeta/Tarjeta';
 import Lista from './Lista/Lista';
@@ -6,11 +6,18 @@ import Buttons from './Buttons/Buttons';
 
 function App() {
   // const nombre = 'Brayan Duque ';
+  const [contador, setContador] = useState(0);
   return (
     <>
       {/* <h1>{nombre}</h1>
       <Tarjeta titulo={'Este es el titulo children'}/> */}
       <Buttons/>
+      <p>
+        Contador: {contador}
+        <button onClick={() => setContador(contador + 1)}>Incrementar</button>
+        <button onClick={() => setContador(contador - 1)}>Decrementar</button>
+      </p>
+
     </>
   );
 }
