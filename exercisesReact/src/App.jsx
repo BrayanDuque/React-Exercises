520ca3ad97c826ca466bacd6ce5566745f091825
Brayan Duque <bradumar2016@gmail.com>
2025-03-16 22:16:55 -0500

    Utilizando estados en formularios

--- exercisesReact/src/App.jsx
+++ exercisesReact/src/App.jsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
-import './App.css';
-import Tarjeta from './Tarjeta/Tarjeta';
-import Lista from './Lista/Lista';
-import Buttons from './Buttons/Buttons';
+import React, { useState } from "react";
+import "./App.css";
+import Tarjeta from "./Tarjeta/Tarjeta";
+import Lista from "./Lista/Lista";
+import Buttons from "./Buttons/Buttons";
 
 function App() {
   // const nombre = 'Brayan Duque ';
-  const [contador, setContador] = useState(0);
+  const [nombre, setNombre] = useState("");
+  function handleNombre(e) {
+    setNombre(e.target.value);
+  }
   return (
     <>
       {/* <h1>{nombre}</h1>
       <Tarjeta titulo={'Este es el titulo children'}/> */}
-      <Buttons/>
-      <p>
-        Contador: {contador}
-        <button onClick={() => setContador(contador + 1)}>Incrementar</button>
-        <button onClick={() => setContador(contador - 1)}>Decrementar</button>
-      </p>
-
+      <Buttons />
+      <p>tu nombre actual es: {nombre}</p>
+      <input type="text" onChange={handleNombre} value={nombre} />
+      <p>{nombre}</p>
     </>
   );
 }
