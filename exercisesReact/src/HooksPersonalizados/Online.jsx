d689d970dad547a057d3b0159be09b4a4af9cd49
Brayan Duque <bradumar2016@gmail.com>
2025-03-24 14:50:18 -0500

    agregando un codigo más corto

--- exercisesReact/src/HooksPersonalizados/Online.jsx
+++ exercisesReact/src/HooksPersonalizados/Online.jsx
@@ -23,3 +23,24 @@ export function useOnlineStatus() {
     //se retorna el estado de inline
   return isOnline;
 }
+
+
+// forma más corta de realizar el hook personalizado realizado en la parte superior 
+// import { useSyncExternalStore } from "react";
+
+// function subscribe(callback) {
+//   window.addEventListener("online", callback);
+//   window.addEventListener("offline", callback);
+//   return () => {
+//     window.removeEventListener("online", callback);
+//     window.removeEventListener("offline", callback);
+//   };
+// }
+
+// export function useOnlineStatus() {
+//   return useSyncExternalStore(
+//     subscribe,
+//     () => navigator.onLine, // How to get the value on the client
+//     () => true // How to get the value on the server
+//   );
+// }
